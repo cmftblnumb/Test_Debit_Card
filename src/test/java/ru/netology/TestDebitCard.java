92f8313ffef4496997420af56575f3eb04dca48f
Kristina P <cmftblnumb@gmail.com>
2025-02-26 09:38:45 -0500

    gradle fixed

--- src/test/java/ru/netology/TestDebitCard.java
+++ src/test/java/ru/netology/TestDebitCard.java
@@ -44,7 +44,7 @@ public class TestDebitCard {
         String actualText = driver.findElement(By.cssSelector("[data-test-id='order-success']")).getText();
         assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", actualText.trim());
 
-
     }
+
 
 }
